12e1120d7de3fe232ec36b67194ae185904dc3fd
Tauno Erik <sedumacre@gmail.com>
2023-12-11 17:53:23 +0200

    start

---
+++ Blink/main.c
@@ -0,0 +1,20 @@
+
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+int main() {
+    stdio_init_all();
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return -1;
+    }
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        printf("LED on\n");
+        sleep_ms(500);
+
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        printf("LED off\n");
+        sleep_ms(500);
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Game Show
+
+- Master
+- Player
+
+## Pico-SDK
+
+cd pico-sdk
+git submodule update --init
+cd ..
+
+cd build
+cmake -DPICO_BOARD=pico_w ..
+make
+
+
